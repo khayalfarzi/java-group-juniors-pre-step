377e6534c8bfa7656e3d9e1653d25987503881c1
Ramil Jafarzade <57438413+ramilsgithub@users.noreply.github.com>
2021-12-23 17:28:28 +0400

    WIP/update:  functionalities added based on readme 1

---
+++ HappyFamily/src/main/java/az/iktlab/pre_step/HappyFamily.java
@@ -0,0 +1,58 @@
+package az.iktlab.pre_step;
+
+import az.iktlab.pre_step.modul.Family;
+import az.iktlab.pre_step.modul.Human;
+import az.iktlab.pre_step.modul.Pet;
+
+import java.util.Scanner;
+
+public class HappyFamily {
+    public static void main(String[] args) {
+
+        Human mother = new Human();
+        mother.setName("Jane ");
+        mother.setSurname("Karleone");
+        mother.setYear(1957);
+
+        Human father = new Human("Vito ", "Karleone", 1955);
+
+        Pet dog = new Pet("dog","Rock", 5,(byte)75 , new String[]{"eat", "drink", "sleep"});
+
+        Human child = new Human("Michael", "Karleone", 1977);
+
+        Human brother = new Human("John", "Karleone", 1980);
+
+        Human child1 = new Human("john", "karlone", 12);
+        Human child2 = new Human("johnyy", "karlone", 9);
+
+        Family family = new Family();
+        family.setMother(mother);
+        family.setFather(father);
+        family.setChildren(new Human[]{child});
+        family.addChild(child1);
+        family.addChild(child2);
+        family.setPet(dog);
+
+        family.deleteChild(0);
+
+        System.out.println(family.toString());
+
+        System.out.println(family.countOfFamily());
+
+        System.out.println("****************Pet class functions*************************");
+        dog.eat();
+        dog.foul();
+        dog.respond();
+        System.out.println("***************************************************************");
+
+        System.out.println("****************Family class functions*************************");
+        family.describePet();
+        family.greetPet();
+        family.feedPet(false);
+        System.out.println("***************************************************************");
+
+
+
+
+    }
+}
---
+++ HappyFamily/src/main/java/az/iktlab/pre_step/modul/Family.java
@@ -0,0 +1,145 @@
+package az.iktlab.pre_step.modul;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Random;
+
+public class Family {
+    private Human mother;
+    private Human father;
+    private Human[] children;
+    private Pet pet;
+
+    public Family() {
+    }
+
+    public Family(Human mother, Human father) {
+        this.mother = mother;
+        this.father = father;
+        children= new Human[]{};
+    }
+
+    public Human getMother() {
+        return mother;
+    }
+
+    public void setMother(Human mother) {
+        this.mother = mother;
+    }
+
+    public Human getFather() {
+        return father;
+    }
+
+    public void setFather(Human father) {
+        this.father = father;
+    }
+
+    public Human[] getChildren() {
+        return children;
+    }
+
+    public void setChildren(Human[] children) {
+        this.children = children;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("Family{ mother=%s%n, father=%s%n, pet = %s%n, children=%s}%n",
+                isNull(getMother()),
+                isNull(getFather()) ,
+                isNull(getPet()),
+                isNull(Arrays.toString(getChildren())) );
+    }
+
+
+    public Object isNull(Object object){
+        return object != null ? object : "empty";
+    }
+
+    public void greetPet(){
+        System.out.println(String.format("Hello, %s",pet.getNickname()) ) ;
+    }
+
+
+    public void describePet(){
+        System.out.println(String.format("I have a %s, he is %s years old, he is %s",pet.getSpecies(),pet.getAge(),
+                new String(pet.getTrickLevel() > 50 ? "very sly" : "almost not sly")));
+
+    }
+
+    public boolean feedPet(Boolean b){
+        if (b==true){
+            System.out.println(String.format("Hm... I will feed %s", pet.getNickname()));
+            return true;
+        }else {
+            Random random = new Random();
+            int r = random.nextInt(100);
+            if (pet.getTrickLevel()>r){
+                System.out.println(String.format("Hm... I will feed %s", pet.getNickname()));
+                return true;}
+            else {
+                System.out.println(String.format("I think &s  is not hungry. %s", pet.getNickname()));
+                return false;
+            }
+        }
+    }
+
+    public void addChild(Human child){
+        Human[] newChild = new Human[getChildren().length+1];
+        for (int i = 0; i < newChild.length-1; i++) {
+            newChild[i] = getChildren()[i];
+        }
+        newChild[newChild.length - 1] = child;
+        setChildren(newChild);
+    }
+
+    public boolean deleteChild(int index){
+        if (index < 0 || index > getChildren().length) return false;
+        else {
+            Human[] deletedChild = new Human[getChildren().length - 1];
+            int k = 0;
+            for (int i = 0; i < getChildren().length; i++) {
+                if (i == index) continue;
+                else {
+                    deletedChild[k] = getChildren()[i];
+                    k++;
+                }
+            }
+            setChildren(deletedChild);
+            return true;
+        }
+    }
+
+    public int countOfFamily(){
+        return getChildren().length + (getFather() != null? 1 : 0) + (getMother() != null? 1 : 0);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Family family = (Family) o;
+        return mother.equals(family.mother) && father.equals(family.father) && Arrays.equals(children, family.children) && pet.equals(family.pet);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(mother, father, pet);
+        result = 31 * result + Arrays.hashCode(children);
+        return result;
+    }
+
+
+
+
+}
